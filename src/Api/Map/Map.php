83c1ec8dcfc0cc2894ae4685be05c48d89d41730
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2016-07-29 19:35:48 +0000

    Preparing for map procedure

--- src/Api/Map/Map.php
+++ src/Api/Map/Map.php
@@ -3,17 +3,39 @@
 namespace NicklasW\PkmGoApi\Api\Map;
 
 use Exception;
+use NicklasW\PkmGoApi\Api\Map\Support\S2;
 use NicklasW\PkmGoApi\Api\Procedure;
+use NicklasW\PkmGoApi\Services\Request\MapRequestService;
 
 class Map extends Procedure {
 
     /**
-     * Returns the map objects.
+     * Returns the map resources.
+     *
+     * @param double $latitude
+     * @param double $longitude
+     * @throws Exception
      */
-    public function getObjects()
+    public function getResources($latitude, $longitude)
     {
+        // Retrieve a list of cell ids from the latitude and longitude
+        $cellIds = S2::getCellIds($latitude, $longitude);
+
+        // Retrieve the map resources
+        $resources = $this->getRequestService()->getResources($latitude, $longitude, $cellIds);
+
+        print_r($resources);
+
+        die();
+
+        
+        
         throw new Exception('To be implemented');
     }
+
+
+
+
 
     /**
      * Returns the catchable pokemons located on the map.
@@ -22,5 +44,23 @@ class Map extends Procedure {
     {
         throw new Exception('To be implemented');
     }
+
+
+
+    /**
+     * Returns the request service.
+     *
+     * @return MapRequestService
+     */
+    protected function getRequestService()
+    {
+        return new MapRequestService();
+    }
+
+
+
+
+
+
 
 }
