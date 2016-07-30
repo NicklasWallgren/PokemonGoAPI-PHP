e3670979b62620bb792f950d8f91d535db98770c
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2016-07-30 06:48:37 +0000

    Updated ProfileData - Updated phpdoc. Updated Currency - Added new method, getByType

--- src/Api/Player/Data/Profile/Currencies.php
+++ src/Api/Player/Data/Profile/Currencies.php
@@ -3,15 +3,25 @@
 
 namespace NicklasW\PkmGoApi\Api\Player\Data\Profile;
 
+use Exception;
 use NicklasW\PkmGoApi\Api\Data\Data;
 use POGOProtos\Data\Player\Currency as PlayerCurrency;
 
 /**
- * @method integer setState(array $state)
- * 
- * @method integer getState()
+ * @method integer setCurrencies(array $state)
+ * @method integer getCurrencies()
  */
 class Currencies extends Data {
+
+    /**
+     * @var string The pokecoin currency type
+     */
+    const CURRENCY_TYPE_POKECOIN = 'POKECOIN';
+
+    /**
+     * @var string The stardust currency type
+     */
+    const CURRENCY_TYPE_STARDUST = 'STARDUST';
 
     /**
      * @var array
@@ -34,11 +44,27 @@ class Currencies extends Data {
             $currency->setName($playerCurrency->getName());
             $currency->setAmount($playerCurrency->getAmount());
 
-            $instance->currencies[] = $currency;
+            $instance->currencies[$playerCurrency->getName()] = $currency;
         }
 
         return $instance;
     }
 
+    /**
+     * Returns the currency by currency type.
+     *
+     * @param string $type
+     * @return mixed
+     * @throws Exception
+     */
+    public function getByType($type)
+    {
+        // Check if the provided type is a valid currency type
+        if (!array_key_exists($type, $this->currencies)) {
+            throw new Exception('Invalid currency type provided');
+        }
+
+        return $this->currencies[$type];
+    }
 
 }
