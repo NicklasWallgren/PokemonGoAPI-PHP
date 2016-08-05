f3e25e18a058521a4e5afb49b4a8c585a9ce41b4
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2016-08-05 15:21:59 +0000

    Updated Pokemon procedure - Added lots of new methods

---
+++ src/Api/Support/MakeApiResourcesAvailable.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace NicklasW\PkmGoApi\Api\Support;
+
+trait MakeApiResourcesAvailable {
+
+    /**
+     * Returns the poke bank.
+     *
+     * @return PokeBank
+     */
+    protected function getPokeBank()
+    {
+        return $this->getPokemonGoApi()->getInventory()->getPokeBank();
+    }
+
+    /**
+     * Returns the candy bank.
+     *
+     * @return Pokedex
+     */
+    protected function getPokedex()
+    {
+        return $this->getInventory()->getPokedex();
+    }
+
+    /**
+     * Returns the candy bank.
+     *
+     * @return CandyBank
+     */
+    protected function getCandyBank()
+    {
+        return $this->getInventory()->getCandyBank();
+    }
+
+    /**
+     * Returns the inventory.
+     *
+     * @return Inventory
+     */
+    protected function getInventory()
+    {
+        return $this->getPokemonGoApi()->getInventory();
+    }
+
+    /**
+     * Returns the pokemon go api.
+     *
+     * @return PokemonGoApi
+     */
+    protected function getPokemonGoApi()
+    {
+        return App::getPokemonGoApi();
+    }
+
+    /**
+     * Returns the application.
+     *
+     * @return ApplicationKernel
+     */
+    protected function getApplication()
+    {
+        return App::getInstance();
+    }
+
+}
