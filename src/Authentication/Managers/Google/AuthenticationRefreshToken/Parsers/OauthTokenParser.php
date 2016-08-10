77d8d628f86e96823bdde1eb4f104fbf1570eaf8
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2016-08-10 15:42:16 +0000

    Updated authentication - Improved error handling

--- src/Authentication/Managers/Google/AuthenticationRefreshToken/Parsers/OauthTokenParser.php
+++ src/Authentication/Managers/Google/AuthenticationRefreshToken/Parsers/OauthTokenParser.php
@@ -2,6 +2,7 @@
 
 namespace NicklasW\PkmGoApi\Authentication\Managers\Google\AuthenticationRefreshToken\Parsers;
 
+use NicklasW\PkmGoApi\Authentication\Exceptions\AuthenticationException;
 use NicklasW\PkmGoApi\Authentication\Managers\Google\AuthenticationRefreshToken\Parsers\Results\AuthenticationTokenResult;
 use NicklasW\PkmGoApi\Facades\Log;
 use PHPHtmlParser\Dom;
@@ -44,5 +45,62 @@ class OauthTokenParser extends Parser {
             )
         );
     }
+
+    /**
+     * Validate the request response.
+     *
+     * @param ResponseInterface $response
+     * @throws ResponseException
+     */
+    protected function validateResponse($response)
+    {
+        Log::debug(sprintf('[#%s] Validating response. Status code: \'%s\'', __CLASS__, $response->getStatusCode()));
+
+        // Check if the response corresponds to a server error
+        if (!$this->isServerError($response)) {
+            return;
+        }
+
+        // Retrieve the response content
+        $content = json_decode((string)$response->getBody());
+
+        // Check if the token is already redeemed
+        if ($this->isTokenAlreadyRedeemed($content)) {
+            throw new AuthenticationException($content->error_description);
+        }
+
+        // Check if the token is invalid
+        if ($this->isTokenInvalid($content)) {
+            throw new AuthenticationException('Invalid token provided');
+        }
+
+        Log::debug(sprintf('[#%s] Retrieved a invalid response. Content: \'%s\'', __CLASS__, $response->getBody()));
+
+        throw new ResponseException(
+            sprintf('Retrieved a invalid response. Response: \'%s\'', (string)$response->getBody()));
+    }
+
+    /**
+     * Check if the token provided is already redeemed.
+     *
+     * @param $content
+     * @return boolean
+     */
+    protected function isTokenAlreadyRedeemed($content)
+    {
+        return $content->error_description === 'Code was already redeemed.';
+    }
+
+    /**
+     * Check if the token provided was invalid.
+     *
+     * @param $content
+     * @return boolean
+     */
+    protected function isTokenInvalid($content)
+    {
+        return $content->error_description === 'Bad Request';
+    }
+
 
 }
