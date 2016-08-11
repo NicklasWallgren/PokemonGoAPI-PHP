4eddf3a0c270867f2c7fe1feff28b89d35b2c30b
Gnacik <gnat@l2ex.pl>
2016-08-11 13:35:39 +0200

    Support for using XP Boost items

---
+++ src/Requests/UseItemXpBoostRequest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace NicklasW\PkmGoApi\Requests;
+
+use POGOProtos\Networking\Envelopes\ResponseEnvelope;
+use POGOProtos\Networking\Requests\Messages\UseItemXpBoostMessage;
+use POGOProtos\Networking\Requests\RequestType;
+use POGOProtos\Networking\Responses\UseItemXpBoostResponse;
+use ProtobufMessage;
+
+class UseItemXpBoostRequest extends Request {
+
+    /**
+     * @var integer The request type
+     */
+    protected $type = RequestType::USE_ITEM_XP_BOOST;
+
+    /**
+     * @var ProtobufMessage The request message
+     */
+    protected $message;
+
+    /**
+     * @var int
+     */
+    protected $itemId;
+
+    /**
+     * UseItemXpBoostRequest constructor.
+     *
+     * @param integer $itemId
+     */
+    public function __construct($itemId)
+    {
+        $this->itemId = $itemId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType()
+    {
+        return RequestType::USE_ITEM_XP_BOOST;
+    }
+
+    /**
+     * @return ProtobufMessage
+     */
+    public function getMessage()
+    {
+        $useItemXpBoostMessage = new UseItemXpBoostMessage();
+        $useItemXpBoostMessage->setItemId($this->itemId);
+
+        return $useItemXpBoostMessage;
+    }
+
+    /**
+     * Handles the request data.
+     *
+     * @param ResponseEnvelope $data
+     * @return UseItemXpBoostResponse
+     */
+    public function handleResponse($data)
+    {
+        // Retrieve the specific request data
+        $requestData = current($data->getReturnsArray());
+
+        // Initialize the rename pokemon response
+        $useItemXpBoostResponse = new UseItemXpBoostResponse();
+
+        // Unmarshall the response
+        $useItemXpBoostResponse->read($requestData);
+
+        $this->setData($useItemXpBoostResponse);
+    }
+}
