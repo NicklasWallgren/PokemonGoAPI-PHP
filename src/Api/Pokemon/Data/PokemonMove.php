90f5cf3829cfa34409da31f3dd8ad3ac6337dbcf
DrDelay <info@vi0lation.de>
2016-08-18 22:44:11 +0200

    Functionality to get all possible moves of a Pokemon

--- src/Api/Pokemon/Data/PokemonMove.php
+++ src/Api/Pokemon/Data/PokemonMove.php
@@ -1,7 +1,46 @@
 <?php
+/**
+ * @author DrDelay <info@vi0lation.de>
+ */
 
 namespace NicklasW\PkmGoApi\Api\Pokemon\Data;
 
-class PokemonMove {
+use POGOProtos\Enums\PokemonMove as PokemonMoveEnum;
 
+class PokemonMove
+{
+    /** @var array|int[]|PokemonMoveEnum[] */
+    protected $moves;
+
+    /**
+     * PokemonMove constructor.
+     *
+     * @param array|int[]|PokemonMoveEnum[] $moves
+     */
+    public function __construct(array $moves)
+    {
+        $this->moves = $moves;
+    }
+
+    /**
+     * Get the moves (raw).
+     *
+     * @return array|\int[]|PokemonMoveEnum[]
+     */
+    public function getMoves()
+    {
+        return $this->moves;
+    }
+
+    /**
+     * Get the moves as names.
+     *
+     * @return string[]
+     */
+    public function getMovesStrings()
+    {
+        return array_map(function ($move) {
+            return PokemonMoveEnum::toString($move);
+        }, $this->getMoves());
+    }
 }
