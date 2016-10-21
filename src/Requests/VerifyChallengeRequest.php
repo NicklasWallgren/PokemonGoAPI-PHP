4cc1a1ae0d804f48f2a4c6071ec3e49c225757d3
voxx <cd@goldencomm.com>
2016-10-21 15:56:14 -0700

    Adding VerifyChallengeRequest classes.

---
+++ src/Requests/VerifyChallengeRequest.php
@@ -0,0 +1,75 @@
+<?php
+namespace NicklasW\PkmGoApi\Requests;
+
+use POGOProtos\Networking\Envelopes\ResponseEnvelope;
+use POGOProtos\Networking\Requests\Messages\VerifyChallenge;
+use POGOProtos\Networking\Requests\RequestType;
+use POGOProtos\Networking\Responses\VerifyChallengeResponse;
+use ProtobufMessage;
+
+class VerifyChallengeRequest extends Request {
+    
+    /**
+     * @var integer The request type
+     */
+    protected $type = RequestType::VERIFY_CHALLENGE;
+    
+    /**
+     * @var ProtobufMessage The request message
+     */
+    protected $message;
+    
+    /**
+     * @var string
+     */
+    protected $token;
+    
+    /**
+     * VerifyChallengeRequest constructor.
+     *
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getType()
+    {
+        return RequestType::VERIFY_CHALLENGE;
+    }
+    
+    /**
+     * @return ProtobufMessage
+     */
+    public function getMessage()
+    {
+        $verifyChallengeMessage = new VerifyChallengeMessage();
+        $verifyChallengeMessage->setToken($this->token);
+        
+        return $verifyChallengeMessage;
+    }
+    
+    /**
+     * Handles the request data.
+     *
+     * @param ResponseEnvelope $data
+     * @return mixed
+     */
+    public function handleResponse($data)
+    {
+        // Retrieve the specific request data
+        $requestData = current($data->getReturnsArray());
+        
+        // Initialize the VerifyChallenge response
+        $verifyChallengeResponse = new VerifyChallengeResponse();
+        
+        // Unmarshall the response
+        $verifyChallengeResponse->read($requestData);
+        
+        $this->setData($verifyChallengeResponse);
+    }
+}
